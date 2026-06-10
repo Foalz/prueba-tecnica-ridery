e4e1fcd71302655832a4d34e331796dd5a59e166
Foalz <pjcontreras20202@gmail.com>
2026-06-10 07:56:41 -0400

    feat: implement automated RIF/Cédula validation for res.partner in Venezuelan localization

--- odoo/addons/l10n_ve_custom/models/res_partner.py
+++ odoo/addons/l10n_ve_custom/models/res_partner.py
@@ -7,12 +7,14 @@ from odoo.exceptions import ValidationError
 # Personas naturales:  V-12345678  o  E-12345678  (7-8 dígitos)
 # Personas jurídicas:  J-123456789              (9 dígitos)
 # Gubernamental:       G-12345678               (8 dígitos)
-# Pasaporte:           P-AB123456               (alfanumérico)
 VE_VAT_REGEX = re.compile(
     r'^[VvEeJjGgPp]-?\d{7,9}$'
 )
 
 VE_COUNTRY_CODE = 'VE'
+
+# Campos que, si cambian, pueden afectar la validación venezolana
+VE_TRIGGER_FIELDS = {'vat', 'country_id'}
 
 
 class ResPartner(models.Model):
@@ -21,6 +23,11 @@ class ResPartner(models.Model):
     is_ve_localization = fields.Boolean(
         string='Localización Venezolana',
         compute='_compute_is_ve_localization',
+    )
+    vat = fields.Char(
+        string='RIF / Cédula',
+        required=True,
+        index=True,
     )
 
     @api.depends('company_id', 'company_id.country_id')
@@ -31,8 +38,11 @@ class ResPartner(models.Model):
                 company.country_id.code == VE_COUNTRY_CODE
             )
 
-    @api.constrains('vat', 'company_id')
-    def _check_ve_vat(self):
+    def _validate_ve_vat(self):
+        """
+        Valida el RIF/Cédula venezolano para todos los partners en self.
+        Solo aplica si la compañía asociada usa localización venezolana (VE).
+        """
         for partner in self:
             company = partner.company_id or self.env.company
             if company.country_id.code != VE_COUNTRY_CODE:
@@ -42,12 +52,11 @@ class ResPartner(models.Model):
                 raise ValidationError(_(
                     "El campo RIF/Cédula es obligatorio para la localización "
                     "venezolana. Por favor ingrese el documento de identidad "
-                    "del contacto '%s'.",
-                    partner.name or ''
+                    "del contacto '%(name)s'.",
+                    name=partner.name or '',
                 ))
 
             vat_clean = partner.vat.strip().upper().replace(' ', '')
-
             if not VE_VAT_REGEX.match(vat_clean):
                 raise ValidationError(_(
                     "El RIF/Cédula '%(vat)s' del contacto '%(name)s' no tiene "
@@ -60,3 +69,15 @@ class ResPartner(models.Model):
                     vat=partner.vat,
                     name=partner.name or '',
                 ))
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        records = super().create(vals_list)
+        records._validate_ve_vat()
+        return records
+
+    def write(self, vals):
+        result = super().write(vals)
+        if VE_TRIGGER_FIELDS & vals.keys():
+            self._validate_ve_vat()
+        return result
